38b018cadf59295b6c78de5f891cf3078ec69256
gaoxiaolei-s59 <2995484417@qq.com>
2026-04-13 15:37:59 +0800

    init: 初始化了基础的rag测试类

---
+++ rag-bootstrap/src/test/java/org/puregxl/site/bootstrap/embedding/EmbeddingDemo.java
@@ -0,0 +1,13 @@
+package org.puregxl.site.bootstrap.embedding;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.net.http.HttpClient;
+
+public class EmbeddingDemo {
+    private static final String API_KEY = "sk-xxxxxxxxxxxxxxxxxxxxxxxx";
+
+    public static void main(String[] args) {
+
+    }
+}
